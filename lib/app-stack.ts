975a6ad8da3d4f2578c67ff0133985e0b413da60
chelorope <cmarcelorp@gmail.com>
2025-02-20 17:02:06 -0300

    Typescript fixes

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,20 +1,14 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 
-const s3 = require("aws-cdk-lib/aws-s3");
-const lambda = require("aws-cdk-lib/aws-lambda");
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
-const { bedrock } = require("@cdklabs/generative-ai-cdk-constructs");
-const { S3EventSource } = require("aws-cdk-lib/aws-lambda-event-sources");
-const iam = require("aws-cdk-lib/aws-iam");
+import * as genAICondtructs from "@cdklabs/generative-ai-cdk-constructs";
+import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
+import * as iam from "aws-cdk-lib/aws-iam";
 
-class AppStack extends cdk.Stack {
-  /**
-   *
-   * @param {Construct} scope
-   * @param {string} id
-   * @param {StackProps=} props
-   */
+export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -25,32 +19,36 @@ class AppStack extends cdk.Stack {
       autoDeleteObjects: true,
     });
 
-    const resumeKnowledgeBase = new bedrock.VectorKnowledgeBase(
+    const resumeKnowledgeBase = new genAICondtructs.bedrock.VectorKnowledgeBase(
       this,
       "resumeKnowledgeBase",
       {
-        embeddingsModel: bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
+        embeddingsModel:
+          genAICondtructs.bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V1,
         instruction:
           "Use this knowledge base to answer questions about resumes",
       }
     );
 
-    const resumeDataSource = new bedrock.S3DataSource(
+    const resumeDataSource = new genAICondtructs.bedrock.S3DataSource(
       this,
       "resumeDataSource",
       {
         bucket: resumeBucket,
         knowledgeBase: resumeKnowledgeBase,
         dataSourceName: "resume",
-        chunkingStrategy: bedrock.ChunkingStrategy.FIXED_SIZE,
-        maxTokens: 500,
-        overlapPercentage: 20,
+        chunkingStrategy: genAICondtructs.bedrock.ChunkingStrategy.FIXED_SIZE,
+        // maxTokens: 500,
+        // overlapPercentage: 20,
       }
     );
 
-    const s3PutEventSource = new S3EventSource(resumeBucket, {
-      events: [s3.EventType.OBJECT_CREATED_PUT],
-    });
+    const s3PutEventSource = new lambdaEventSources.S3EventSource(
+      resumeBucket,
+      {
+        events: [s3.EventType.OBJECT_CREATED_PUT],
+      }
+    );
 
     /*********  Ingestion Job *********/
 
